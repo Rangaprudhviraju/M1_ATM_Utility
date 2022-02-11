bb0aa56823c31c5d578ba5523d8192952825b47d
Rangaprudhviraju <82215129+Rangaprudhviraju@users.noreply.github.com>
2022-02-11 13:11:31 +0530

    Update Money_Deposit.c

--- 3_Implementation/src/Money_Deposit.c
+++ 3_Implementation/src/Money_Deposit.c
@@ -1,1 +1,33 @@
 
+/**
+ * @file Money_Deposit.c
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include"atm.h"
+
+float moneyDeposit(float balance) {
+    float deposit;
+    printf("You choose to Deposit a money\n");
+    printf("Your Balance is: $%.2f\n\n", balance);
+    printf("****Enter your amount to Deposit\n");
+    scanf("%f", &deposit);
+
+
+    balance += deposit;
+
+    printf("\n****Your New Balance is:   $%.2f\n\n", balance);
+    return balance;
+
+}//Money deposit
