6a8099c93f108fe0459af64438a9a59f84acb862
Rangaprudhviraju <82215129+Rangaprudhviraju@users.noreply.github.com>
2022-02-11 13:16:39 +0530

    Create Invalid.c

---
+++ 3_Implementation/src/Invalid.c
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include"atm.h"
+
+ 
+
+   int errorMessage() 
+{
+    printf("+++!!!You selected invalid number!!!+++\n");
+    return 9;
+}
