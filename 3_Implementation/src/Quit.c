794a365d87847617a867f3cbf05bbeb3f829edae
Rangaprudhviraju <82215129+Rangaprudhviraju@users.noreply.github.com>
2022-02-11 13:21:13 +0530

    Create Quit.c

---
+++ 3_Implementation/src/Quit.c
@@ -0,0 +1,23 @@
+/**
+ * @file Quit.c
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include"atm.h"
+
+int menuExit() {
+    printf("--------------Take your receipt!!!------------------\n");
+    printf("-----Thank you for using ATM Banking Machine!!!-----\n");
+    return 5;
+
+}//exit menu
